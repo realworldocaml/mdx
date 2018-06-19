9b43de9250cb6856e59d49daa368b149b0e0a2f9
Anil Madhavapeddy <anil@recoil.org>
2018-06-19 15:33:21 +0100

    test: do not do a shallow clone so we can push the repo to github
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- test/vendor-rwo.sh
+++ test/vendor-rwo.sh
@@ -3,7 +3,7 @@
 PACKAGES="core_extended async atdgen toplevel_expect_test patdiff sexp_pretty re fmt ppxlib lambdasoup cmdliner ppx_tools"
 dune build
 rm -rf _rwo
-git clone --depth=1 git://github.com/realworldocaml/book _rwo
+git clone git://github.com/realworldocaml/book _rwo
 dune exec -- duniverse opam-lock $PACKAGES -vv
 dune exec -- duniverse dune-lock -vv
 dune exec -- duniverse dune-fetch -r _rwo -vv
