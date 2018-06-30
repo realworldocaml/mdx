bae9e8a105da3152cb999042d24c6630be89da82
Anil Madhavapeddy <anil@recoil.org>
2018-06-30 12:46:53 +0100

    add travis tests

---
+++ .docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DISTRO=${DISTRO:-alpine-3.7}
+
+set -ex
+# TODO opam2 depext
+case $DISTRO in
+alpine-*) sudo apk add m4 ;;
+debian-*) sudo apt update; sudo apt -y install m4 pkg-config ;;
+ubuntu-*) sudo apt update; sudo apt -y install m4 pkg-config ;;
+esac
+
+sudo chown -R opam /home/opam/src
+cd /home/opam/src
+make
