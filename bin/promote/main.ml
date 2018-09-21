01aba0076f86ef6737ac91853c0777f074f2de74
Guillaume Petiot <guillpetiot@gmail.com>
2018-09-21 15:49:10 +0200

    Fix headers

--- bin/promote/main.ml
+++ bin/promote/main.ml
@@ -1,5 +1,6 @@
 (*
  * Copyright (c) 2018 Thomas Gazagnaire <thomas@gazagnaire.org>
+ *                    Guillaume Petiot <guillaume@tarides.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
