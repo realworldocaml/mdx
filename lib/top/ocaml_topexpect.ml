7eeb4da58372473ca3e709bce5df89d604c5c4cd
Guillaume Petiot <guillpetiot@gmail.com>
2018-09-28 10:07:59 +0200

    cleaning

--- lib/top/ocaml_topexpect.ml
+++ lib/top/ocaml_topexpect.ml
@@ -24,10 +24,6 @@ module Chunk = struct
 
   let v ~ocaml_code ~toplevel_responses = {ocaml_code; toplevel_responses}
   let code c = c.ocaml_code
-  let _warnings (_ : t) : string =  ""
-  let _responses c = c.toplevel_responses
-  let _stdout (_ : t) = ""
-  let _evaluated (_ : t) = true
 end
 
 module Part = struct
@@ -46,7 +42,6 @@ module Document = struct
 
   let v ~parts ~matched = {parts; matched}
   let parts {parts;_} = parts
-  let _matched {matched;_} = matched
 end
 
 module Lexbuf = struct
@@ -107,14 +102,6 @@ module Lexbuf = struct
     in
     aux
 
-  let _position_mapper start =
-    let open Ast_mapper in
-    let start = {start with pos_fname = toplevel_fname} in
-    let location mapper loc =
-      shift_toplevel_location ~start (default_mapper.location mapper loc)
-    in
-    {default_mapper with location}
-
 end
 
 module Phrase = struct
@@ -129,9 +116,6 @@ module Phrase = struct
     endpos   : position;
     parsed   : (toplevel_phrase, exn) result;
   }
-
-  let _result t = t.parsed
-  let _start t = t.startpos
 
   let read lexbuf =
     let startpos = lexbuf.Lexing.lex_curr_p in
@@ -255,14 +239,6 @@ module Phrase = struct
     let start = skip_whitespace doc.Lexbuf.contents ~stop start in
     String.sub doc.contents start (stop - start)
 
-  let whitespace doc phrase rest =
-    let start = phrase.endpos.pos_cnum in
-    let stop = match rest with
-      | [] -> String.length doc.Lexbuf.contents
-      | (p, _) :: _ -> skip_whitespace doc.contents p.startpos.pos_cnum
-    in
-    String.sub doc.contents start (stop - start)
-
   let document doc ~matched phrases =
     let rec parts_of_phrase part acc = function
       | (_, Part { name; _ }) :: rest ->
@@ -282,15 +258,6 @@ module Phrase = struct
     in
     let parts = parts_of_phrase "" [] phrases in
     Document.v ~matched ~parts
-
-  let _is_findlib_directive =
-    let findlib_directive = function
-      | "require" | "use" | "camlp4o" | "camlp4r" | "thread" -> true
-      | _ -> false
-    in
-    function
-    | { parsed = Ok (Ptop_dir (dir, _)); _ } -> findlib_directive dir
-    | _ -> false
 
   let dry_exec phrases =
     let rec aux acc = function
@@ -312,217 +279,6 @@ module Phrase = struct
       | Some phrase -> aux ((phrase, kind phrase) :: phrases)
     in
     dry_exec (aux [])
-
-  let find_delim s =
-    let len = String.length s in
-    let delims = ref [] in
-    let beginning = ref (-1) in
-    for i = 0 to len - 1 do
-      match s.[i] with
-      | '{' -> beginning := i
-      | '|' when !beginning = -1 || s.[!beginning] <> '{' -> beginning := i
-      | ('|' | '}' as c) when !beginning <> -1 && (c = '|' || s.[!beginning] = '|') ->
-        let delim = String.sub s (!beginning + 1) (i - !beginning - 1) in
-        begin match !delims with
-          | delim' :: _ when delim' = delim -> ()
-          | delims' -> delims := delim :: delims'
-        end;
-        beginning := -1
-      | 'a'..'z' | '_' -> ()
-      | _ -> beginning := -1
-    done;
-    let delims = !delims in
-    let candidates = [""; "escape"; "x"; "y"; "z"] in
-    match List.find (fun delim -> not (List.mem delim delims)) candidates with
-    | candidate -> candidate
-    | exception Not_found ->
-      (* Generate a string that is not in the list of delimiters *)
-      let next b =
-        try
-          for i = Bytes.length b - 1 downto 0 do
-            match Bytes.get b i with
-            | 'z' -> Bytes.set b i 'a'
-            | c ->
-              Bytes.set b i (Char.chr (Char.code c + 1));
-              raise Exit
-          done;
-          Bytes.cat b (Bytes.unsafe_of_string "a")
-        with Exit -> b
-      in
-      let rec exhaust b =
-        if not (List.mem (Bytes.unsafe_to_string b) delims)
-        then Bytes.unsafe_to_string b
-        else exhaust (next b)
-      in
-      exhaust (Bytes.of_string "")
-
-let is_whitespace = function
-  | ' ' | '\n' -> true
-  | _ -> false
-
-let is_all_whitespace str =
-  try
-    for i = 0 to String.length str - 1 do
-      if not (is_whitespace str.[i]) then raise Exit
-    done;
-    true
-  with Exit -> false
-
-let is_ellision_line str =
-  let i = ref 0 in
-  let j = String.length str in
-  while !i < j && is_whitespace str.[!i] do incr i done;
-  !i <= j - 3 && str.[!i] = '.' && str.[!i+1] = '.' && str.[!i+2] = '.' && (
-    i := !i + 3;
-    while !i < j && is_whitespace str.[!i] do incr i done;
-    !i = j
-  )
-
-let string_subequal subject str i =
-  let len = String.length subject in
-  String.length str >= len + i &&
-  try
-    for j = 0 to len - 1 do
-      if subject.[j] <> str.[i+j] then
-        raise Exit;
-    done;
-    true
-  with Exit ->
-    false
-
-let match_outcome_chunk (k1,outcome) (k2,expected) =
-  k1 = k2 &&
-  let rec split_chunks acc str i0 i =
-    match String.index_from str i '\n' with
-    | exception Not_found ->
-      let acc =
-        if is_ellision_line (String.sub str i (String.length str - i))
-        then "" :: String.sub str i0 i :: acc
-        else String.sub str i0 (String.length str - i0) :: acc
-      in
-      List.rev acc
-    | j ->
-      if is_ellision_line (String.sub str i (j - i)) then
-        split_chunks (String.sub str i0 (i - i0) :: acc) str (j + 1) (j + 1)
-      else
-        split_chunks acc str i0 (j + 1)
-  in
-  match split_chunks [] expected 0 0 with
-  | [] -> assert false
-  | x :: xs ->
-    string_subequal x outcome 0 &&
-    let rec match_chunks i = function
-      | [] -> i = String.length outcome
-      | [x] ->
-        let i' = String.length outcome - String.length x in
-        i' >= i && string_subequal x outcome i'
-      | x :: xs ->
-        let bound = String.length outcome - String.length x in
-        let i = ref i in
-        while !i <= bound && not (string_subequal x outcome !i)
-        do incr i done;
-        if !i > bound then false
-        else match_chunks (!i + String.length x) xs
-    in
-    match_chunks (String.length x) xs
-
-let match_outcome xs ys =
-  List.length xs = List.length ys &&
-  List.for_all2 match_outcome_chunk xs ys
-
-(* Check if output matches expectations and keep ellisions when
-   possible *)
-let _validate ~run_nondeterministic =
-  let rec aux success acc = function
-    | [] -> (success, List.rev acc)
-    | (_, Part _ as entry) :: rest ->
-      aux success (entry :: acc) rest
-    | (p0, Code outcome) :: (p1, Expect outcome') :: rest ->
-      let success' =
-        if outcome'.nondeterministic && not run_nondeterministic then
-          true
-        else
-          match_outcome outcome outcome'.responses
-      in
-      let acc =
-        if success' then
-          (p1, Expect outcome') :: (p0, Code outcome'.responses) :: acc
-        else
-          (p1, Expect outcome') :: (p0, Code outcome) :: acc
-      in
-      aux (success && success') acc rest
-    | (_, Code outcome as x) :: rest ->
-      let success =
-        success && List.for_all (fun (_,s) -> is_all_whitespace s) outcome
-      in
-      aux success (x :: acc) rest
-    | (_, Expect _ as x) :: rest ->
-      aux false (x :: acc) rest
-  in
-  fun phrases -> aux true [] phrases
-
-let _output oc lexbuf =
-  let rec aux = function
-    | [] -> ()
-    | (phrase, Part {name; location}) :: rest ->
-      Printf.fprintf oc "%s[@@@part %S];;\n"
-        (contents lexbuf phrase ~stop:location.loc_start.pos_cnum) name;
-      aux rest
-    | (phrase, Code expect_code) :: rest ->
-      let phrase_post, expect_pre, expect_post, nondeterministic, rest =
-        match rest with
-        | (phrase_expect, Expect x) :: rest' ->
-          (whitespace lexbuf phrase rest,
-           contents lexbuf phrase_expect ~stop:x.location.loc_start.pos_cnum,
-           whitespace lexbuf phrase_expect rest',
-           x.nondeterministic,
-           rest')
-        | _ ->
-          ("\n", "", whitespace lexbuf phrase rest, false, rest)
-      in
-      let phrase_code = contents lexbuf phrase in
-      if List.for_all (fun (_,s) -> is_all_whitespace s) expect_code &&
-         not nondeterministic then
-        Printf.fprintf oc "%s%s%s" phrase_code expect_pre expect_post
-      else (
-        let string_of_kind = function
-          | Chunk.Raw -> ""
-          | Chunk.OCaml -> "ocaml "
-        in
-        let output_expect oc = function
-          | [] -> ()
-          | [(kind, str)] when not (String.contains str '\n') ->
-            let k = string_of_kind kind in
-            let delim = find_delim str in
-            Printf.fprintf oc "%s%s{%s|%s|%s}" (if k <> "" then " " else "") k delim str delim
-          | xs ->
-            let rec aux first = function
-              | [] -> ()
-              | (k,s) :: xs ->
-                let k = string_of_kind k in
-                let pre = if first then (if k <> "" then " " else "") else "\n" in
-                let post = if xs = [] then "" else ";" in
-                let delim = find_delim s in
-                if not (String.contains s '\n') then
-                  Printf.fprintf oc "%s%s{%s|%s|%s}%s" pre k delim s delim post
-                else
-                  Printf.fprintf oc "%s%s{%s|\n%s\n|%s}%s" pre k delim s delim post;
-                aux false xs
-            in
-            aux true xs
-        in
-        Printf.fprintf oc "%s%s%s[%%%%expect%s%a];;%s"
-          phrase_code phrase_post
-          expect_pre
-          (if nondeterministic then ".nondeterministic" else "")
-          output_expect expect_code expect_post;
-      );
-      aux rest
-    | (phrase, Expect {location; _}) :: rest ->
-      Printf.fprintf oc "%s"
-        (contents lexbuf phrase ~stop:location.loc_start.pos_cnum);
-      aux rest
-  in aux
 
 end
 
