cbfc9207d48a4d894f9d8186970ed6d77a736417
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-04-10 18:15:22 +0200

    Use latest debian, alpine and ubuntu distros

--- .docker.sh
+++ .docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-DISTRO=${DISTRO:-alpine-3.7}
+DISTRO=${DISTRO:-alpine-3.9}
 
 set -ex
 # TODO opam2 depext
 case $DISTRO in
-alpine-*) sudo apk add m4 ;;
-debian-*) sudo apt update; sudo apt -y install m4 pkg-config ;;
-ubuntu-*) sudo apt update; sudo apt -y install m4 pkg-config ;;
+alpine*) sudo apk add m4 ;;
+debian*) sudo apt update; sudo apt -y install m4 pkg-config ;;
+ubuntu*) sudo apt update; sudo apt -y install m4 pkg-config ;;
 esac
 
 sudo chown -R opam /home/opam/src
