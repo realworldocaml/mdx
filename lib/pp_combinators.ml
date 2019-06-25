cbdfd58e6a1e9e3bfe0d37b80066e892d6730aca
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-06-25 13:36:58 +0200

    Add a space before closing brackets when pretty-printing ocaml lists

--- lib/pp_combinators.ml
+++ lib/pp_combinators.ml
@@ -11,5 +11,5 @@ module Ocaml = struct
     | [ a ] -> Format.fprintf fmt "[%a]" pp_a a
     | l ->
         let pp_sep fmt () = Format.fprintf fmt ";@ " in
-        Format.fprintf fmt "@[<hov 2>[@ %a]@]" (Format.pp_print_list ~pp_sep pp_a) l
+        Format.fprintf fmt "@[<hov 2>[@ %a@ ]@]" (Format.pp_print_list ~pp_sep pp_a) l
 end
