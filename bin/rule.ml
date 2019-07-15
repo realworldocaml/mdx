ae4d4a8a96a6bb17c1cc218d154c5c63ad225d4a
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-07-15 11:33:06 +0200

    Add a dependency on mdx package in mdx generated rules
    This prevents error due to duplicate binary from happening when
    vendoring mdx. It's a quick fix rather than a long term solution since
    technically we still need to make `ocaml-mdx` depend on
    `ocaml-mdx-test`.

--- bin/rule.ml
+++ bin/rule.ml
@@ -79,7 +79,8 @@ let print_rule ~nd ~prelude ~md_file ~ml_files ~dirs ~root options =
       "\
 (alias\n\
 \ (name   %s)\n\
-\ (deps   (:x %s)%s)\n\
+\ (deps   (:x %s)\n\
+\         (package mdx)%s)\n\
 \ (action (progn\n\
 \           (run ocaml-mdx test %a %s%s%%{x})\n\
 \           (diff? %%{x} %%{x}.corrected)\n%a)))\n"
