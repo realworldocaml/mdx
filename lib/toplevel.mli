85276e542d62bace185787dcb680a6892df9c330
Guillaume Petiot <guillpetiot@gmail.com>
2020-01-29 17:51:02 +0700

    update doc of Toplevel.of_lines

--- lib/toplevel.mli
+++ lib/toplevel.mli
@@ -42,7 +42,7 @@ val pp_command: t Fmt.t
 (** {2 Parser} *)
 
 val of_lines: syntax:Syntax.t -> file:string -> line:int -> string list -> t list
-(** [of_lines ~file ~line lines] is the list of toplevel blocks from
+(** [of_lines ~syntax ~file ~line lines] is the list of toplevel blocks from
    file [file] starting at line [line]. Return the vertical and
    horizontal whitespace padding as well.*)
 
