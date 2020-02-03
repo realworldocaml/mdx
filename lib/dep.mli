ceae5b983d361c82c063274493157a9709c2e98c
Ulysse Gérard <thevoodoos@gmail.com>
2020-02-03 13:48:30 +0100

    And again. (typo in dep.ml·i)

--- lib/dep.mli
+++ lib/dep.mli
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2018 Ulysse Gérard <ulysse@gtarides.com>
+ * Copyright (c) 2020 Ulysse Gérard <ulysse@tarides.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
