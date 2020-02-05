f0ba952b28c67edd6b1217106cef58b1d20ff9cf
Guillaume Petiot <guillpetiot@gmail.com>
2020-02-05 18:15:43 +0700

    Do not use Astring.String.cut and other Astring functions

--- lib/label.ml
+++ lib/label.ml
@@ -14,8 +14,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-open Astring
-
 type relation = Eq | Neq | Le | Lt | Ge | Gt
 
 type t =
@@ -54,20 +52,21 @@ let pp ppf = function
   | Set (v, x) -> Fmt.pf ppf "set-%s=%s" v x
   | Unset x -> Fmt.pf ppf "unset-%s" x
 
-let split sep s op =
-  match String.cut ~sep s with
-  | None -> s, None (* operator does not matter here *)
-  | Some (k, v) -> k, Some (op, v)
-
 let ( ||| ) x y =
   match x with
   | _, None -> y
   | x -> x
 
+let is_prefix ~prefix s =
+  let len_prefix = String.length prefix in
+  if String.length s > len_prefix then
+    String.equal (String.sub s 0 len_prefix) prefix
+  else false
+
+(* [is_prefix ~prefix s] is always checked before. *)
 let split_prefix ~prefix s =
-  match String.cut ~sep:prefix s with
-  | Some ("", v) when not (String.is_empty v) -> v
-  | _ -> Fmt.failwith "invalid label `%s`" s
+  let len_prefix = String.length prefix in
+  String.sub s len_prefix (String.length s - len_prefix)
 
 let doesnt_accept_value ~label ~value res =
   match value with
@@ -86,20 +85,38 @@ let requires_eq_value ~label ~value f =
     Fmt.failwith "label `%s` requires assignment using the `=` operator" label
   | None -> Fmt.failwith "label `%s` requires a value" label
 
+let raw_parse_1_char op op_char s =
+  match String.index_opt s op_char with
+  | Some i ->
+    let before = String.sub s 0 i in
+    let after = String.sub s (i+1) (String.length s - i - 1) in
+    (before, Some (op, after))
+  | None -> (s, None)
+
+let raw_parse_2_chars op op_char_1 op_char_2 s =
+  match String.index_opt s op_char_1 with
+  | Some i
+    when String.length s > i + 2 && Char.equal (String.get s (i+1)) op_char_2 ->
+    let before = String.sub s 0 i in
+    let after = String.sub s (i+2) (String.length s - i - 2) in
+    (before, Some (op, after))
+  | _ -> (s, None)
+
+let raw_parse s =
+  raw_parse_2_chars Neq '<' '>' s |||
+  raw_parse_2_chars Ge '>' '=' s |||
+  raw_parse_1_char Gt '>' s |||
+  raw_parse_2_chars Le '<' '=' s |||
+  raw_parse_1_char Lt '<' s |||
+  raw_parse_1_char Eq '=' s
+
 let of_string s =
-  let label, value =
-    split "<>" s Neq |||
-    split ">=" s Ge |||
-    split ">"  s Gt |||
-    split "<=" s Le |||
-    split "<"  s Lt |||
-    split "="  s Eq
-  in
+  let label, value = raw_parse s in
   match label with
   (* flags: labels without value *)
   | "skip" ->
     doesnt_accept_value ~label ~value Skip
-  | v when String.is_prefix ~affix:"unset-" v ->
+  | v when is_prefix ~prefix:"unset-" v ->
     doesnt_accept_value ~label ~value (Unset (split_prefix ~prefix:"unset-" v))
   (* labels requiring a value and any comparison operator *)
   | "version" -> (
@@ -130,16 +147,14 @@ let of_string s =
   | "env" -> requires_eq_value ~label ~value (fun x -> Env x)
   | "require-package" ->
     requires_eq_value ~label ~value (fun x -> Require_package x)
-  | l when String.is_prefix ~affix:"set-" l ->
+  | l when is_prefix ~prefix:"set-" l ->
     requires_eq_value ~label ~value
       (fun x -> Set (split_prefix ~prefix:"set-" l, x))
   | l -> Fmt.failwith "`%s` is not a valid label" l
 
-let of_string s =
-  if String.is_empty s then []
-  else
-    let labels = String.cuts ~empty:false ~sep:"," s in
-    List.map of_string labels
+let of_string = function
+  | "" -> []
+  | s -> List.map of_string (String.split_on_char ',' s)
 
 let relation_compare = function
   | Eq -> ( = )
