1588a8b2aade5ca4735fcacbe9c0369ff35c95c5
Anil Madhavapeddy <anil@recoil.org>
2020-04-13 14:19:00 +0100

    sync duniverse with latest deps

---
+++ duniverse/alcotest.1.1.0/src/alcotest/utils.mli
@@ -0,0 +1,25 @@
+module List : sig
+  include module type of List
+
+  type 'a t = 'a list
+
+  val filter_map : ('a -> 'b option) -> 'a list -> 'b list
+
+  val lift_result : ('a, 'b) result t -> ('a t, 'b t) result
+end
+
+module Result : sig
+  val map : ('a -> 'b) -> ('a, 'e) result -> ('b, 'e) result
+end
+
+module Unix : sig
+  include module type of Unix
+
+  val mkdir_p : string -> file_perm -> unit
+end
+
+module Fmt : sig
+  include module type of Fmt
+
+  val flush : 'a t
+end
