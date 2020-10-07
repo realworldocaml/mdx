27964255876bdc992c1aac8dbd85e052be120a8c
Nathan Rebours <nathan.p.rebours@gmail.com>
2020-10-07 14:35:16 +0200

    Remove out of date integration tests

--- .docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -z "$OCAMLFORMAT" ]; then
-  docker build --build-arg DISTRO=$DISTRO -f .dockerfiles/opam .
-else
-  docker build --build-arg OCAMLFORMAT=$OCAMLFORMAT -f .dockerfiles/ocamlformat .
-fi
--- examples/basic/basic.ml
+++
@@ -1,3 +0,0 @@
-let () =
-  let _ = Rresult.Ok () in
-  Fmt.pf Format.std_formatter "hello!\n"
