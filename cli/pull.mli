b25bbf797e3d134c7db4091942b359b76783a35a
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-03-03 14:55:05 +0100

    Unexpose unused Pull.run
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- cli/pull.mli
+++ cli/pull.mli
@@ -1,8 +1,1 @@
 val cmd : unit Cmdliner.Term.t * Cmdliner.Term.info
-
-val run :
-  [< `Yes of bool ] ->
-  [< `Repo of Fpath.t ] ->
-  [< `Duniverse_repos of string list option ] ->
-  unit ->
-  (unit, Rresult.R.msg) result
