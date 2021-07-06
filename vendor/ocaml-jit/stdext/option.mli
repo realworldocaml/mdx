9d89fe6a6dedaff3b61f68b95fcaf25bec3b72dd
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-07-06 11:32:09 +0200

    Unvendor JIT, opttopfind and native toplevel and use pins instead
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- vendor/ocaml-jit/stdext/option.mli
+++
@@ -1,9 +0,0 @@
-include module type of struct
-  include Stdlib.Option
-end
-
-module Op : sig
-  val ( let* ) : 'a option -> ('a -> 'b option) -> 'b option
-
-  val ( let+ ) : 'a option -> ('a -> 'b) -> 'b option
-end
