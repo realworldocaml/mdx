49259a24b5c1f1d62d197187e69afd118290c460
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-07-06 11:32:09 +0200

    Unvendor JIT, opttopfind and native toplevel and use pins instead
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- vendor/ocaml-jit/clitop/clitop.mli
+++
@@ -1,6 +0,0 @@
-val main :
-  name:string ->
-  eval_phrase:(bool -> Format.formatter -> Parsetree.toplevel_phrase -> bool) ->
-  loop:(Format.formatter -> unit) ->
-  unit ->
-  unit
--- vendor/ocaml-jit/stdext/string.mli
+++
@@ -1,7 +0,0 @@
-include module type of struct
-  include StringLabels
-end
-
-module Map : Map.S with type key = string
-
-val starts_with : prefix:string -> string -> bool
