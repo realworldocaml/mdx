8054037deccc8b50e3e0755a0bbf4976f2360aab
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-10-29 11:24:59 +0200

    Rename Repo to Project
    opam-monorepo does not actually need a git repository to work
    but rather a dune workspace or project. This feels like a better
    terminology to use to avoid confusion among the opam-monorepo
    devs
    
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- lib/project.mli
+++ lib/project.mli
@@ -1,7 +1,10 @@
 open Import
-(** Utility functions to extract repository specific path and values *)
+(** Utility functions to extract project specific path and values *)
 
 type t = Fpath.t
+(** The type of projects.
+    
+    What we consider a project here is the root of a dune project/workspace *)
 
 val local_packages :
   recurse:bool ->
@@ -16,19 +19,19 @@ val local_packages :
 val dune_project : t -> Fpath.t
 (** Returns the path to the dune-project file. *)
 
-val project_name : t -> (string, [> `Msg of string ]) result
+val name : t -> (string, [> `Msg of string ]) result
 (** Returns the name of the project, as set in the dune-project. *)
 
 val lockfile :
   ?local_packages:OpamPackage.Name.t list ->
   t ->
   (Fpath.t, [> `Msg of string ]) result
-(** Returns the path to the opam-monorepo lockfile for the given repo.
+(** Returns the path to the opam-monorepo lockfile for the given project.
     If the repo contains a single package, then it's the ["<package_name>.opam.locked"]
-    file at the root of the repo.
+    file at the root of the project.
     If it contains multiple packages, then it's the ["<project_name>.opam.locked"] file
-    at the root of the repo.
-    One can provide [local_packages] if they were already computed are if only a subset
+    at the root of the project.
+    One can provide [local_packages] if they were already computed or if only a subset
     of the local packages must be taken into account. *)
 
 val local_lockfiles : t -> (Fpath.t list, Rresult.R.msg) result
