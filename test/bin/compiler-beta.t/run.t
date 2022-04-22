tarides/opam-monorepo

Fix test to prevent duplicate pkg definition warning
Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

test/bin/compiler-beta.t/run.t
@@ -1,6 +1,6 @@
 We have a simple project that only depends on OCaml
 
-  $ opam show --no-lint -fdepends ./test.opam
+  $ opam show --no-lint -fdepends ./dont-want-beta.opam
   "ocaml" "dune"
 
 We use the minimal repository which includes OCaml 4.13.1
@@ -20,8 +20,8 @@ Our package does not define an upper bound on ocaml but the beta should not be
 selected by default if there exists another solution, therefore here the solver
 should pick 4.13.1:
 
-  $ opam-monorepo lock test > /dev/null
-  $ opam show --no-lint -fdepends ./test.opam.locked | grep ocaml
+  $ opam-monorepo lock dont-want-beta > /dev/null
+  $ opam show --no-lint -fdepends ./dont-want-beta.opam.locked | grep ocaml
   "ocaml" {= "4.13.1"}
   "ocaml-base-compiler" {= "4.13.1"}
   "ocaml-config" {= "2"}
@@ -29,14 +29,14 @@ should pick 4.13.1:
 
 Now if we require ocaml >= 4.14, as the following package does:
 
-  $ opam show --no-lint -fdepends ./test-requires-beta.opam
+  $ opam show --no-lint -fdepends ./requires-beta.opam
   "ocaml" {>= "4.14"} "dune"
 
 The solver should be able to select the beta compiler since it is the only
 satisfying version available:
 
-  $ opam-monorepo lock test-requires-beta > /dev/null
-  $ opam show --no-lint -fdepends ./test-requires-beta.opam.locked | grep ocaml
+  $ opam-monorepo lock requires-beta > /dev/null
+  $ opam show --no-lint -fdepends ./requires-beta.opam.locked | grep ocaml
   "ocaml" {= "4.14.0"}
   "ocaml-base-compiler" {= "4.14.0~beta1"}
   "ocaml-config" {= "2"}

test/bin/depext.t/run.t
@@ -6,7 +6,7 @@ We setup the default base repository
 
 Here we define a package test that depends on a package `b`:
 
-  $ opam show --no-lint --raw -fdepends ./a.opam
+  $ opam show --no-lint --raw -fdepends ./depext.opam
   "dune" "b"
 
 Package `b` has a `depext` on a fantasy OS package. We deliberately pick a
@@ -17,12 +17,12 @@ fantasy name here to make sure the behaviour is the same on all platforms.
 
 We lock and expect the OS package to be part of the locked Opam file.
 
-  $ opam-monorepo lock a > /dev/null
-  $ opam show --no-lint --raw -fdepexts ./a.opam.locked
+  $ opam-monorepo lock > /dev/null
+  $ opam show --no-lint --raw -fdepexts ./depext.opam.locked
   "libfantasydependency"
 
 Then we want to make sure `depext` works. Given the fantasy package does not
 exist, `depext` should not install any packages but still work successfully:
 
   $ opam-monorepo depext --dry-run
-  ==> Using lockfile $TESTCASE_ROOT/a.opam.locked
+  ==> Using lockfile $TESTCASE_ROOT/depext.opam.locked

test/bin/require-cross-compiling-packages.t/run.t
@@ -34,19 +34,19 @@ maintainers created a 0.1+dune+mirage port in mirage-opam-overlays:
 You'll note the "cross-compile" tag that we use to mark packages that can be
 cross compiled in a dune-workspace.
 
-For testing purposes we define two opam files: a.opam and a-with-mirage.opam
+For testing purposes we define two opam files: without-mirage.opam and with-mirage.opam
 that are essencially the same: they both depends on b but the latter configures
 the solver to use the mirage overlays in addition with upstream and
 dune-universe:
 
-  $ opam show --no-lint -fdepends ./a.opam
+  $ opam show --no-lint -fdepends ./without-mirage.opam
   "dune" "b"
-  $ opam show --no-lint -fx-opam-monorepo-opam-repositories --raw ./a.opam
+  $ opam show --no-lint -fx-opam-monorepo-opam-repositories --raw ./without-mirage.opam
   "file://$OPAM_MONOREPO_CWD/opam-repository"
   "file://$OPAM_MONOREPO_CWD/opam-overlays"
-  $ opam show --no-lint -fdepends ./a-with-mirage.opam
+  $ opam show --no-lint -fdepends ./with-mirage.opam
   "dune" "b"
-  $ opam show --no-lint -fx-opam-monorepo-opam-repositories --raw ./a-with-mirage.opam
+  $ opam show --no-lint -fx-opam-monorepo-opam-repositories --raw ./with-mirage.opam
   "file://$OPAM_MONOREPO_CWD/opam-repository"
   "file://$OPAM_MONOREPO_CWD/opam-overlays"
   "file://$OPAM_MONOREPO_CWD/mirage-opam-overlays"
@@ -54,15 +54,15 @@ dune-universe:
 Until there is a new release, everything goes fine. If we don't add the mirage
 overlays the solver picks the dune port as expected:
 
-  $ opam-monorepo lock a > /dev/null
-  $ opam show --no-lint -fdepends ./a.opam.locked | grep "\"b\""
+  $ opam-monorepo lock without-mirage > /dev/null
+  $ opam show --no-lint -fdepends ./without-mirage.opam.locked | grep "\"b\""
   "b" {= "0.1+dune" & ?vendor}
 
 If we add the mirage overlays, the mirage port gets picked instead as its
 version is higher (+mirage):
 
-  $ opam-monorepo lock a-with-mirage > /dev/null
-  $ opam show --no-lint -fdepends ./a-with-mirage.opam.locked | grep "\"b\""
+  $ opam-monorepo lock with-mirage > /dev/null
+  $ opam show --no-lint -fdepends ./with-mirage.opam.locked | grep "\"b\""
   "b" {= "0.1+dune+mirage" & ?vendor}
 
 So far so good. Problems arise when a new release of b hits upstream. We managed
@@ -82,16 +82,16 @@ to upstream the dune port before `0.2` so the `0.2` release builds with dune.
 
 Regular users of opam-monorepo will get the 0.2 version and be happy with it:
 
-  $ opam-monorepo lock a > /dev/null
-  $ opam show --no-lint -fdepends ./a.opam.locked | grep "\"b\""
+  $ opam-monorepo lock without-mirage > /dev/null
+  $ opam show --no-lint -fdepends ./without-mirage.opam.locked | grep "\"b\""
   "b" {= "0.2" & ?vendor}
 
 Mirage users on the other hand will get it as well, meaning they can't cross
 compile their unikernel anymore. The solver is happy but this will cause errors
 at build time for them:
 
-  $ opam-monorepo lock a-with-mirage > /dev/null
-  $ grep "\"b\"\s\+{" a-with-mirage.opam.locked
+  $ opam-monorepo lock with-mirage > /dev/null
+  $ grep "\"b\"\s\+{" with-mirage.opam.locked
     "b" {= "0.2" & ?vendor}
 
 We added the --require-cross-compile flag to select packages that cross compile
@@ -99,21 +99,21 @@ when available.
 Here, if we don't add mirage overlays and run the solver with this flag, we
 still get the latest release:
 
-  $ opam-monorepo lock --require-cross-compile a > /dev/null
-  $ opam show --no-lint -fdepends ./a.opam.locked | grep "\"b\""
+  $ opam-monorepo lock --require-cross-compile without-mirage > /dev/null
+  $ opam show --no-lint -fdepends ./without-mirage.opam.locked | grep "\"b\""
   "b" {= "0.2" & ?vendor}
 
 If we run it with mirage overlays though, it will detect that there exist
 versions that cross compile and favor those instead:
 
-  $ opam-monorepo lock --require-cross-compile a-with-mirage > /dev/null
-  $ opam show --no-lint -fdepends ./a-with-mirage.opam.locked | grep "\"b\""
+  $ opam-monorepo lock --require-cross-compile with-mirage > /dev/null
+  $ opam show --no-lint -fdepends ./with-mirage.opam.locked | grep "\"b\""
   "b" {= "0.1+dune+mirage" & ?vendor}
 
 Note that if the upstream released version does cross compile, it can add the
 tag to be picked instead:
 
   $ echo "tags: [\"cross-compile\"]" >> opam-repository/packages/b/b.0.2/opam
-  $ opam-monorepo lock --require-cross-compile a-with-mirage > /dev/null
-  $ opam show --no-lint -fdepends ./a-with-mirage.opam.locked | grep "\"b\""
+  $ opam-monorepo lock --require-cross-compile with-mirage > /dev/null
+  $ opam show --no-lint -fdepends ./with-mirage.opam.locked | grep "\"b\""
   "b" {= "0.2" & ?vendor}

realworldocaml/mdx

test/bin/compiler-beta.t/run.t
@@ -1,6 +1,6 @@
 We have a simple project that only depends on OCaml
 
-  $ opam show --just-file -fdepends ./test.opam
+  $ opam show --just-file -fdepends ./dont-want-beta.opam
   ocaml, dune
 
 We use the minimal repository which includes OCaml 4.13.1
@@ -20,8 +20,8 @@ Our package does not define an upper bound on ocaml but the beta should not be
 selected by default if there exists another solution, therefore here the solver
 should pick 4.13.1:
 
-  $ opam-monorepo lock test > /dev/null
-  $ opam show --just-file -fdepends ./test.opam.locked | grep ocaml
+  $ opam-monorepo lock dont-want-beta > /dev/null
+  $ opam show --just-file -fdepends ./dont-want-beta.opam.locked | grep ocaml
   "ocaml" {= "4.13.1"}
   "ocaml-base-compiler" {= "4.13.1"}
   "ocaml-config" {= "2"}
@@ -29,14 +29,14 @@ should pick 4.13.1:
 
 Now if we require ocaml >= 4.14, as the following package does:
 
-  $ opam show --just-file -fdepends ./test-requires-beta.opam
+  $ opam show --just-file -fdepends ./requires-beta.opam
   "ocaml" {>= "4.14"} "dune"
 
 The solver should be able to select the beta compiler since it is the only
 satisfying version available:
 
-  $ opam-monorepo lock test-requires-beta > /dev/null
-  $ opam show --just-file -fdepends ./test-requires-beta.opam.locked | grep ocaml
+  $ opam-monorepo lock requires-beta > /dev/null
+  $ opam show --just-file -fdepends ./requires-beta.opam.locked | grep ocaml
   "ocaml" {= "4.14.0"}
   "ocaml-base-compiler" {= "4.14.0~beta1"}
   "ocaml-config" {= "2"}

test/bin/depext.t/run.t
@@ -6,7 +6,7 @@ We setup the default base repository
 
 Here we define a package test that depends on a package `b`:
 
-  $ opam show --just-file --raw -fdepends ./a.opam
+  $ opam show --just-file --raw -fdepends ./depext.opam
   dune, b
 
 Package `b` has a `depext` on a fantasy OS package. We deliberately pick a
@@ -17,12 +17,12 @@ fantasy name here to make sure the behaviour is the same on all platforms.
 
 We lock and expect the OS package to be part of the locked Opam file.
 
-  $ opam-monorepo lock a > /dev/null
-  $ opam show --just-file --raw -fdepexts ./a.opam.locked
+  $ opam-monorepo lock > /dev/null
+  $ opam show --just-file --raw -fdepexts ./depext.opam.locked
   libfantasydependency
 
 Then we want to make sure `depext` works. Given the fantasy package does not
 exist, `depext` should not install any packages but still work successfully:
 
   $ opam-monorepo depext --dry-run
-  ==> Using lockfile $TESTCASE_ROOT/a.opam.locked
+  ==> Using lockfile $TESTCASE_ROOT/depext.opam.locked

test/bin/require-cross-compiling-packages.t/run.t
@@ -34,32 +34,32 @@ maintainers created a 0.1+dune+mirage port in mirage-opam-overlays:
 You'll note the "cross-compile" tag that we use to mark packages that can be
 cross compiled in a dune-workspace.
 
-For testing purposes we define two opam files: a.opam and a-with-mirage.opam
+For testing purposes we define two opam files: without-mirage.opam and with-mirage.opam
 that are essencially the same: they both depends on b but the latter configures
 the solver to use the mirage overlays in addition with upstream and
 dune-universe:
 
-  $ opam show --just-file -fdepends ./a.opam
+  $ opam show --just-file -fdepends ./without-mirage.opam
   dune, b
-  $ opam show --just-file -fx-opam-monorepo-opam-repositories --raw ./a.opam
+  $ opam show --just-file -fx-opam-monorepo-opam-repositories --raw ./without-mirage.opam
   file://$OPAM_MONOREPO_CWD/opam-repository, file://$OPAM_MONOREPO_CWD/opam-overlays
-  $ opam show --just-file -fdepends ./a-with-mirage.opam
+  $ opam show --just-file -fdepends ./with-mirage.opam
   dune, b
-  $ opam show --just-file -fx-opam-monorepo-opam-repositories --raw ./a-with-mirage.opam
+  $ opam show --just-file -fx-opam-monorepo-opam-repositories --raw ./with-mirage.opam
   file://$OPAM_MONOREPO_CWD/opam-repository, file://$OPAM_MONOREPO_CWD/opam-overlays, file://$OPAM_MONOREPO_CWD/mirage-opam-overlays
 
 Until there is a new release, everything goes fine. If we don't add the mirage
 overlays the solver picks the dune port as expected:
 
-  $ opam-monorepo lock a > /dev/null
-  $ opam show --just-file -fdepends ./a.opam.locked | grep "\"b\""
+  $ opam-monorepo lock without-mirage > /dev/null
+  $ opam show --just-file -fdepends ./without-mirage.opam.locked | grep "\"b\""
   "b" {= "0.1+dune" & ?vendor}
 
 If we add the mirage overlays, the mirage port gets picked instead as its
 version is higher (+mirage):
 
-  $ opam-monorepo lock a-with-mirage > /dev/null
-  $ opam show --just-file -fdepends ./a-with-mirage.opam.locked | grep "\"b\""
+  $ opam-monorepo lock with-mirage > /dev/null
+  $ opam show --just-file -fdepends ./with-mirage.opam.locked | grep "\"b\""
   "b" {= "0.1+dune+mirage" & ?vendor}
 
 So far so good. Problems arise when a new release of b hits upstream. We managed
@@ -79,16 +79,16 @@ to upstream the dune port before `0.2` so the `0.2` release builds with dune.
 
 Regular users of opam-monorepo will get the 0.2 version and be happy with it:
 
-  $ opam-monorepo lock a > /dev/null
-  $ opam show --just-file -fdepends ./a.opam.locked | grep "\"b\""
+  $ opam-monorepo lock without-mirage > /dev/null
+  $ opam show --just-file -fdepends ./without-mirage.opam.locked | grep "\"b\""
   "b" {= "0.2" & ?vendor}
 
 Mirage users on the other hand will get it as well, meaning they can't cross
 compile their unikernel anymore. The solver is happy but this will cause errors
 at build time for them:
 
-  $ opam-monorepo lock a-with-mirage > /dev/null
-  $ grep "\"b\"\s\+{" a-with-mirage.opam.locked
+  $ opam-monorepo lock with-mirage > /dev/null
+  $ grep "\"b\"\s\+{" with-mirage.opam.locked
     "b" {= "0.2" & ?vendor}
 
 We added the --require-cross-compile flag to select packages that cross compile
@@ -96,21 +96,21 @@ when available.
 Here, if we don't add mirage overlays and run the solver with this flag, we
 still get the latest release:
 
-  $ opam-monorepo lock --require-cross-compile a > /dev/null
-  $ opam show --just-file -fdepends ./a.opam.locked | grep "\"b\""
+  $ opam-monorepo lock --require-cross-compile without-mirage > /dev/null
+  $ opam show --just-file -fdepends ./without-mirage.opam.locked | grep "\"b\""
   "b" {= "0.2" & ?vendor}
 
 If we run it with mirage overlays though, it will detect that there exist
 versions that cross compile and favor those instead:
 
-  $ opam-monorepo lock --require-cross-compile a-with-mirage > /dev/null
-  $ opam show --just-file -fdepends ./a-with-mirage.opam.locked | grep "\"b\""
+  $ opam-monorepo lock --require-cross-compile with-mirage > /dev/null
+  $ opam show --just-file -fdepends ./with-mirage.opam.locked | grep "\"b\""
   "b" {= "0.1+dune+mirage" & ?vendor}
 
 Note that if the upstream released version does cross compile, it can add the
 tag to be picked instead:
 
   $ echo "tags: [\"cross-compile\"]" >> opam-repository/packages/b/b.0.2/opam
-  $ opam-monorepo lock --require-cross-compile a-with-mirage > /dev/null
-  $ opam show --just-file -fdepends ./a-with-mirage.opam.locked | grep "\"b\""
+  $ opam-monorepo lock --require-cross-compile with-mirage > /dev/null
+  $ opam show --just-file -fdepends ./with-mirage.opam.locked | grep "\"b\""
   "b" {= "0.2" & ?vendor}