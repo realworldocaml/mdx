8644d701dc28c33e972714aa030a31a5fd6eeb0d
Anil Madhavapeddy <anil@recoil.org>
2026-07-21 18:28:04 +0000

    use fancy dune rules to do a more robust oxcaml check

--- lib/top/gen_cppo_flags.ml
+++ lib/top/gen_cppo_flags.ml
@@ -1,12 +1,6 @@
-(* OxCaml compilers report a version of the form X.Y.Z+ox *)
+(* OxCaml compilers answer "true" to [ocamlopt -config-var ox] *)
 let () =
-  let version = Sys.argv.(1) in
-  let is_oxcaml =
-    (* When min supported version is ocaml>=4.13 we can use String.ends_with *)
-    match String.index_opt version '+' with
-    | Some i ->
-        let build = String.sub version (i + 1) (String.length version - i - 1) in
-        String.length build >= 2 && String.sub build 0 2 = "ox"
-    | None -> false
-  in
-  if is_oxcaml then print_string "-D\nOXCAML\n"
+  let ic = open_in_bin Sys.argv.(1) in
+  let contents = really_input_string ic (in_channel_length ic) in
+  close_in ic;
+  if String.trim contents = "true" then print_string "-D\nOXCAML\n"
