147d5c4ef7e029e3c514283d744175a4ab8fa0b4
Anil Madhavapeddy <anil@recoil.org>
2026-07-22 06:42:04 +0000

    use dune 3.21's ox detection to not require a discover.ml

--- lib/top/gen_cppo_flags.ml
+++
@@ -1,6 +0,0 @@
-(* OxCaml compilers answer "true" to [ocamlopt -config-var ox] *)
-let () =
-  let ic = open_in_bin Sys.argv.(1) in
-  let contents = really_input_string ic (in_channel_length ic) in
-  close_in ic;
-  if String.trim contents = "true" then print_string "-D\nOXCAML\n"
